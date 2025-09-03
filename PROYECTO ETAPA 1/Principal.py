9eed4ba2960602eb2356fb97911da2dbbf8a6ee2
Mateo Vallejo <106411815+MateoVallejo06@users.noreply.github.com>
2025-09-02 21:12:55 -0300

    Add files via upload

---
+++ PROYECTO ETAPA 1/Principal.py
@@ -0,0 +1,12 @@
+"""Proyecto etapa 1"""
+import Funciones
+def main():
+    #iniciar matriz por comprension
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
+
+
---
+++ PROYECTO ETAPA 1/README.md
@@ -0,0 +1,1 @@
+# Tp-Programaci-n-1
